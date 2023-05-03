6b893dd10cf9e962901a2991b436a37080f65756
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 03:09:13 -0500

    feat: telescope config

---
+++ lua/plugins/config/telescope.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "kyazdani42/nvim-web-devicons" },
+  },
+  keys = {
+    { "<leader>f",  function() require("telescope.builtin").find_files() end },
+    { "<leader>a",  function() require("telescope.builtin").live_grep() end },
+    { "<leader>mf", function() require("telescope.builtin").marks() end },
+    { "<leader>ls", function() require("telescope.builtin").git_commits() end },
+    { "<leader>ag", function() require("telescope.builtin").git_files() end },
+    { "<leader>of", function() require("telescope.builtin").oldfiles() end },
+    { "<leader>b",  function() require("telescope.builtin").buffers() end },
+  },
+  config = function()
+    require('telescope').setup {
+      initial_mode = "insert",
+      defaults = {
+        layout_config = {
+          vertical = { width = 0.5 }
+        },
+        file_ignore_patterns = { "node_modules", ".git" }
+      },
+      pickers = {
+        find_files = {
+          theme = "dropdown",
+          hidden = true
+        },
+        oldfiles = {
+          theme = "ivy"
+        },
+        live_grep = {
+        },
+        git_commits = {
+          theme = "ivy"
+        },
+        buffers = {
+          ignore_current_buffer = true,
+          sort_mru = true,
+          theme = "dropdown"
+        }
+      },
+      extensions = {
+      }
+    }
+  end
+}
