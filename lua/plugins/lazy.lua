9ca736390cff15dd93c1b56118375d277cf2f700
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 03:05:55 -0500

    feat: configuracion de `lazy.lua` :wrench:

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -1,5 +1,4 @@
 --plugins/lazy => configuraciones iniciales de lazy
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -13,4 +12,31 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins.configs")
+require("lazy").setup({
+  spec = {
+    {
+      import = "plugins.config",
+    },
+    {
+      import = "themes",
+    },
+  },
+  default = { lazy = true, version = false },
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
