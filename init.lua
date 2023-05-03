01f536c7a63b49b505a68abd2dbc72b4cab9a140
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 02:39:27 -0500

    feat!: archivo `init.lua` :package:

--- init.lua
+++ init.lua
@@ -1,2 +1,23 @@
-local core = require('core')
-local lazy = require('plugins.lazy')
+local core = require("core")
+local lazy = require("plugins.lazy")
+
+--[[
+  TODO:
+ - [] Terminar la confuración de todos los plugins restantes
+ - [x] Hacer la configuración de autoformateo de go y js
+ - [] Documentación en Español
+ - [x] Realizar el menu de temas
+ - [x] Mejorar la configuración de lazy
+ - [x] Configuracion de cmp inicial
+ - [x] Configuracion de algunos plugins de treesitter
+ - [x] Autopairs plugin de treesitter
+ PLUGINS:
+  -x git-signs
+  -x vim-visual-multi
+  -x markdown-preview
+  - debuger
+
+ FIX: -[x] Error en los TABS de cmp
+      - Esta viendo la carpera node_modules
+--]]
+--
