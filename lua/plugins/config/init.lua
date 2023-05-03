a3cd3e1230f6e2a7eedca22fca61b710baa02f38
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 03:05:12 -0500

    feat: config de los plugins que no necesitan configuracion

---
+++ lua/plugins/config/init.lua
@@ -0,0 +1,9 @@
+--plugins/config/init => Carga los plugins que no necesitan configuración
+
+return {
+  "lilydjwg/colorizer",
+  "tpope/vim-fugitive",
+  "tpope/vim-surround",
+  "matze/vim-move",
+  "mg979/vim-visual-multi", --Requiere configuracion
+}
