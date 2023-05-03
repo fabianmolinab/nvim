ed9716df70cba9b02ee1bb00b9b2c6644d390aff
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 03:15:28 -0500

    chore: plugins de markdown preview #35 :sparkles:

---
+++ lua/plugins/config/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  build = "cd app && npm install",
+  ft = "markdown",
+  init = function()
+    vim.api.nvim_set_var("mkdp_auto_close", 0)
+    vim.api.nvim_set_var("mkdp_refresh_slow", 1)
+    vim.api.nvim_set_var("mkdp_browser", "edge")
+    vim.api.nvim_set_var("mkdp_echo_preview_url", 1)
+    vim.api.nvim_set_var("mkdp_theme", "dark")
+  end,
+}
