3ece7c3c86d98547a477df8fd04214cee0e8b4c9
fabianmolinab <molinafabian11b@gmail.com>
2023-05-03 14:18:23 -0500

    chore: actualizando el TODO :white_check_mark:

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 local core = require("core")
 local lazy = require("plugins.lazy")
 
+  
 --[[
   TODO:
- - [] Terminar la confuración de todos los plugins restantes
+  - [] Documentación en Español
+ - [x] Terminar la confuración de todos los plugins restantes
  - [x] Hacer la configuración de autoformateo de go y js
- - [] Documentación en Español
  - [x] Realizar el menu de temas
  - [x] Mejorar la configuración de lazy
  - [x] Configuracion de cmp inicial
@@ -15,9 +16,9 @@ local lazy = require("plugins.lazy")
   -x git-signs
   -x vim-visual-multi
   -x markdown-preview
-  - debuger
+  -x debuger
 
  FIX: -[x] Error en los TABS de cmp
-      - Esta viendo la carpera node_modules
+      - Esta viendo la carpeta node_modules
 --]]
 --
