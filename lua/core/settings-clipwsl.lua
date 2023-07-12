5dee1093e447a3ef4765d0c800e79270454405c9
fabianmolinab <molinafabian11b@gmail.com>
2023-07-11 20:26:46 -0500

    feat: se agrego el clipboard para wsl

---
+++ lua/core/settings-clipwsl.lua
@@ -0,0 +1,14 @@
+--Permite Copiar y pegar por fuera y por dentro en WSL2
+local g = vim.g
+g.clipboard = {
+    name = 'WslClipboard',
+     copy = {
+        ['+'] = 'clip.exe',
+        ['*'] = 'clip.exe',
+    },
+    paste = {
+        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+}
