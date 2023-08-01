69a1b801bb5d1084c90a83b6332e7ec97cabb470
fabianmolinab <molinafabian11b@gmail.com>
2023-08-01 10:03:02 -0500

    feat: empezando la configuracion de java

---
+++ lua/plugins/config/java.lua
@@ -0,0 +1,7 @@
+return {
+    "mfussenegger/nvim-jdtls",
+    dependencies = { "mfussenegger/nvim-dap", "neovim/nvim-lspconfig" },
+    event = "VeryLazy",
+    config = function()
+              end
+}
