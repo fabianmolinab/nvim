305db15b19f69eb32bc3add51866d941e4be2f9e
fabianmolinab <molinafabian11b@gmail.com>
2023-11-02 00:03:09 -0500

    fix: migracion v3 black-indent

--- lua/plugins/config/black-indent.lua
+++ lua/plugins/config/black-indent.lua
@@ -1,33 +1,20 @@
-local open_file_event = { "BufNewFile", "BufRead", "BufAdd", "SessionLoadPost" }
 local globals = require('globals')
 return {
   "lukas-reineke/indent-blankline.nvim",
+  cmd = { "IBLDisable", "IBLEnable", "IBLToggle", "IBLDisableScope", "IBLEnableScope", "IBLToggleScope" },
   main = "ibl", opts = {},
   depedencies = "nvim-treesitter/nvim-treesitter",
-  event = open_file_event,
+  event = "FileType",
   config = function()
-    --vim.cmd [[highlight IndentBlanklineIndent guifg=#22262F ]]
     globals.opt.list = true
-    --vim.opt.listchars:append "trail:●"
-    --vim.opt.listchars:append "space:·"
-    --vim.opt.listchars:append "eol:↓"
-
     require("ibl").setup({
-      use_treesitter = true,
-      show_current_context = true,
-      show_current_context_start = true,
-      show_trailing_blankline_indent = false,
-      show_end_of_line = true,
-      space_char_blankline = " ",
-      buftype_exclude = {
-        "help",
-        "loclist",
-        "nofile",
-        "prompt",
-        "quickfix",
-        "terminal",
-      },
-      filetype_exclude = { "list" },
+        debounce = globals.a.nvim_get_option_value("updatetime", { scope = "global" }),
+        indent = { char = "▏", priority = 11 },
+        scope = { enabled = false },
+        exclude = {
+          filetypes = { "list", "netrw", "" },
+          buftypes = { "help", "loclist", "nofile", "prompt", "quickfix", "terminal" },
+        },
     })
   end
 }
