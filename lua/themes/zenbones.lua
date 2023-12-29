1810db83006d39d3d1704a3b51518e8ccf384cf9
fabianmolinab <molinafabian11b@gmail.com>
2023-12-29 01:58:10 -0500

    fix: changes in some themes

--- lua/themes/zenbones.lua
+++ lua/themes/zenbones.lua
@@ -1,29 +1,29 @@
 return {
-  "mcchrish/zenbones.nvim",
-  dependencies = "rktjmp/lush.nvim",
-  lazy = false,
-  enabled = true,
-  priority = 1000,
-  config = function()
-    vim.cmd 'set background=dark'
+    "mcchrish/zenbones.nvim",
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    enabled = false,
+    priority = 1000,
+    config = function()
+        vim.cmd 'set background=dark'
 
-    -- vim.g.zenbones = { transparent_background = true }
-    -- vim.cmd "colorscheme zenbones"
-    -- vim.g.zenwritten = { transparent_background = true }
-    -- vim.cmd "colorscheme zenwritten"
+        -- vim.g.zenbones = { transparent_background = true }
+        -- vim.cmd "colorscheme zenbones"
+        -- vim.g.zenwritten = { transparent_background = true }
+        -- vim.cmd "colorscheme zenwritten"
 
-    -- vim.cmd "colorscheme rosebones"
-    -- vim.g.rosebones = { transparent_background = true }
+        -- vim.cmd "colorscheme rosebones"
+        -- vim.g.rosebones = { transparent_background = true }
 
-    vim.g.nordbones = {
-      transparent_background = true,
-      italic_comments = false
-    }
-    vim.cmd "colorscheme nordbones"
+        vim.g.nordbones = {
+            transparent_background = true,
+            italic_comments = false
+        }
+        vim.cmd "colorscheme nordbones"
 
-    -- vim.cmd "colorscheme neobones"
-    -- vim.cmd "colorscheme tokyobones"
-    -- vim.cmd "colorscheme kanagawabones"
-    -- vim.cmd "colorscheme randombones"
-  end
+        -- vim.cmd "colorscheme neobones"
+        -- vim.cmd "colorscheme tokyobones"
+        -- vim.cmd "colorscheme kanagawabones"
+        -- vim.cmd "colorscheme randombones"
+    end
 }
