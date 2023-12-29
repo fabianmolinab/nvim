f51b8c6fda95d9c72ab3f14bef00da964a1dde0d
fabianmolinab <molinafabian11b@gmail.com>
2023-12-29 01:55:52 -0500

    feat: remplace neo-tree for telescope_file_browser
    se reemplazo el plugin de buscado de archivos en arbol por el telescope_file_browser

--- lua/plugins/config/telescope.lua
+++ lua/plugins/config/telescope.lua
@@ -1,80 +1,72 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    { "nvim-lua/plenary.nvim" }, { "nvim-tree/nvim-web-devicons" }
-    -- {"nvim-telescope/telescope-file-browser.nvim"}
-  },
-  config = function()
-    -- local fb_actions = require("telescope._extensions.file_browser.actions")
-    require('telescope').setup {
-      initial_mode = "insert",
-      defaults = {
-        layout_config = { vertical = { width = 0.5 } },
-        file_ignore_patterns = { "node_modules", ".git" }
-      },
-      pickers = {
-        find_files = { theme = "dropdown", hidden = true },
-        oldfiles = { theme = "ivy" },
-        live_grep = {},
-        git_commits = { theme = "ivy" },
-        git_status = {
-          git_icons = {
-            added = "A",
-            deleted = "D",
-            changed = "M",
-            renamed = "R",
-            copied = "C",
-            untracked = "U",
-            unmerged = "?"
-          }
-        },
-        buffers = {
-          initial_mode = "normal",
-          ignore_current_buffer = true,
-          sort_mru = true,
-          theme = "dropdown"
-        }
-      },
-      extensions = {
-        --[[
-        file_browser = {
-          initial_mode = "normal",
-          defaults = {
-            layout_config = {
-              vertical = { width = 0.5 }
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        {"nvim-lua/plenary.nvim"}, {"nvim-tree/nvim-web-devicons"},
+        {"nvim-telescope/telescope-file-browser.nvim"}
+    },
+    config = function()
+        local fb_actions = require("telescope._extensions.file_browser.actions")
+        require('telescope').setup {
+            initial_mode = "insert",
+            defaults = {
+                layout_config = {vertical = {width = 0.5}},
+                file_ignore_patterns = {"node_modules", ".git"}
             },
-            file_ignore_patterns = { "node_modules", ".git" }
-          },
-          theme = "dropdown",
-          cwd_to_path = true,
-          grouped = true,
-          hidden = true,
-          display_stat = {
-            date = false, mode = false
-          },
-          git_icons = {
-            added = "A",
-            deleted = "D",
-            changed = "M",
-            renamed = "R",
-            copied = "C",
-            untracked = "u",
-            unmerged = "?",
-          },
-          mappings = {
-            ["n"] = {
-              ["a"] = fb_actions.create,
-              ["c"] = fb_actions.copy,
-              ["x"] = fb_actions.remove,
+            pickers = {
+                find_files = {theme = "dropdown", hidden = true},
+                oldfiles = {theme = "ivy"},
+                live_grep = {},
+                git_commits = {theme = "ivy"},
+                git_status = {
+                    git_icons = {
+                        added = "A",
+                        deleted = "D",
+                        changed = "M",
+                        renamed = "R",
+                        copied = "C",
+                        untracked = "U",
+                        unmerged = "?"
+                    }
+                },
+                buffers = {
+                    initial_mode = "normal",
+                    ignore_current_buffer = true,
+                    sort_mru = true,
+                    theme = "dropdown"
+                }
+            },
+            extensions = {
+                file_browser = {
+                    initial_mode = "normal",
+                    defaults = {
+                        layout_config = {vertical = {width = 0.5}},
+                        file_ignore_patterns = {"node_modules", ".git"}
+                    },
+                    theme = "dropdown",
+                    cwd_to_path = true,
+                    grouped = true,
+                    hidden = true,
+                    display_stat = {date = true, mode = false},
+                    git_icons = {
+                        added = "A",
+                        deleted = "D",
+                        changed = "M",
+                        renamed = "R",
+                        copied = "C",
+                        untracked = "u",
+                        unmerged = "?"
+                    },
+                    mappings = {
+                        ["n"] = {
+                            ["a"] = fb_actions.create,
+                            ["c"] = fb_actions.copy,
+                            ["x"] = fb_actions.remove
+                        }
+                    }
+                }
             }
-          }
-        },
-        ]]
-      }
-    }
-    --[[
-    require("telescope").load_extension "file_browser"
-    vim.cmd("autocmd VimEnter * :Telescope file_browser")
-    ]]
-  end
+        }
+        -- require("telescope").load_extension "file_browser"
+        -- vim.cmd("autocmd VimEnter * :Telescope file_browser")
+    end
 }
