2e4b1a138d5c9a4cbc27b150660562e304b40c9e
fabianmolinab <molinafabian11b@gmail.com>
2024-04-14 16:17:47 -0500

    fix: config icon diagnostics lsp

--- lua/plugins/config/lsp/diagnostics.lua
+++ lua/plugins/config/lsp/diagnostics.lua
@@ -1,11 +1,22 @@
 local M = {}
 M.setup = function ()
 
-    local signs = {Error = "", Warn = "", Hint = "", Info = ""}
-    for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = hl})
-    end
+    vim.fn.sign_define(
+    "DiagnosticSignError",
+    { text = " ", texthl = "DiagnosticSignError" }
+    )
+    vim.fn.sign_define(
+    "DiagnosticSignWarn",
+    { text = " ", texthl = "DiagnosticSignWarn" }
+    )
+    vim.fn.sign_define(
+    "DiagnosticSignInfo",
+    { text = " ", texthl = "DiagnosticSignInfo" }
+    )
+    vim.fn.sign_define(
+    "DiagnosticSignHint",
+    { text = "󰌵", texthl = "DiagnosticSignHint" }
+    )
 
     vim.diagnostic.config({
         underline = true,
@@ -16,6 +27,6 @@ M.setup = function ()
     })
 
     -- Show line diagnostics automatically in hover window
-    vim.o.updatetime = 250
+    vim.o.updatetime = 200
 end
 return M
