eefced47fc7cb8c25b60f99cb0821cbbe905aa84
fabianmolinab <molinafabian11b@gmail.com>
2024-06-08 01:55:50 -0500

    fix: config de temas y colores

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -1,11 +1,12 @@
 local globals = require('globals')
 -- Configurar termguicolors y background
 globals.opt.termguicolors = true
-globals.opt.background = 'dark'
+globals.opt.background = 'light'
+
 
 -- Configurar cursorline y cursorlineopt
 globals.opt.cursorline = true
 globals.opt.cursorlineopt = 'number'
 
 -- Configurar menú de autocompletado a trasparente
-globals.cmd('highlight Pmenu guifg=#dee1e4 guibg=none')
+--globals.cmd('highlight Pmenu guifg=#dee1e4 guibg=none')
--- lua/themes/gruvboxMaterial.lua
+++ lua/themes/gruvboxMaterial.lua
@@ -1,12 +1,12 @@
 return {
     "sainnhe/gruvbox-material",
     lazy = false,
-    enabled = true,
+    enabled = false,
     priority = 1000,
     config = function()
-        vim.g.gruvbox_material_background = 'hard'
-        vim.g.gruvbox_material_enable_bold = true
-        vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_background = 'solft'
+        --vim.g.gruvbox_material_enable_bold = true
+        --vim.g.gruvbox_material_enable_italic = true
         vim.g.gruvbox_material_transparent_background = false
 
         vim.cmd('colorscheme gruvbox-material')
