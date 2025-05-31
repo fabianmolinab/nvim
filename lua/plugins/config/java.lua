2eecb73d853a82d193b5df8305a5249063d4a7af
fabianmolinab <molinafabian11b@gmail.com>
2025-05-31 13:21:37 -0500

    fix: correccion error de lsp_java

--- lua/plugins/config/java.lua
+++ lua/plugins/config/java.lua
@@ -1,26 +1,3 @@
 return {
   "nvim-java/nvim-java",
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        setup = {
-          jdtls = function()
-            require("java").setup({
-               root_markers = {
-                 "settings.gradle",
-                 "settings.gradle.kts",
-                 "pom.xml",
-                 "build.gradle",
-                 "mvnw",
-                 "gradlew",
-                 "build.gradle",
-                 "build.gradle.kts",
-               },
-            })
-          end,
-        },
-      },
-    },
-  },
 }
