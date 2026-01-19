827640f5618a684a8388f15f17f8b6abc6203310
fabianmolinab <molinafabian11b@gmail.com>
2026-01-19 04:53:59 -0500

    fix: metodos deprecados de config diagnosticos
    - Se reasigno la función para mostrar los iconos de advertencias en lsp

--- lua/plugins/config/lsp/diagnostics.lua
+++ lua/plugins/config/lsp/diagnostics.lua
@@ -1,24 +1,15 @@
 local M = {}
 M.setup = function ()
 
-    vim.fn.sign_define(
-    "DiagnosticSignError",
-    { text = " ", texthl = "DiagnosticSignError" }
-    )
-    vim.fn.sign_define(
-    "DiagnosticSignWarn",
-    { text = " ", texthl = "DiagnosticSignWarn" }
-    )
-    vim.fn.sign_define(
-    "DiagnosticSignInfo",
-    { text = " ", texthl = "DiagnosticSignInfo" }
-    )
-    vim.fn.sign_define(
-    "DiagnosticSignHint",
-    { text = "󰌵", texthl = "DiagnosticSignHint" }
-    )
-
     vim.diagnostic.config({
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = " ",
+                [vim.diagnostic.severity.WARN] = " ",
+                [vim.diagnostic.severity.INFO] = " ",
+                [vim.diagnostic.severity.HINT] = "󰌵",
+            },
+        },
         underline = true,
         virtual_text = false,
         update_in_insert = false,
@@ -26,7 +17,6 @@ M.setup = function ()
         float = {border = 'rounded', source = 'always'}
     })
 
-    -- Show line diagnostics automatically in hover window
     vim.o.updatetime = 200
 end
 return M
